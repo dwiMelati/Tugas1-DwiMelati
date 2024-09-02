694d25f4c080441c5cf90fb0fef600da5e475462
dwiMelati <dwip05736@gmail.com>
2024-09-02 16:45:57 +0700

    komit pertama

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Profil from './Profil';
+
+export default function App() {
+  return (
+    <div>
+      <h1>
+        Pahlawan Indonesia
+      </h1>
+      <Profil/>
+    </div>
+  );
+}
---
+++ src/Profil.jsx
@@ -0,0 +1,27 @@
+export default function Profil() {
+  return (
+    <div className="profil">
+      <div className="pahlawan">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Presiden_Sukarno.jpg/800px-Presiden_Sukarno.jpg"
+          alt="Soekarno"
+        />
+        <p>Ir.Soekarno</p>
+      </div>
+      <div className="pahlawan">
+        <img
+          src="https://akcdn.detik.net.id/community/media/visual/2022/08/14/mohammad-hatta-1.jpeg?w=700&q=90"
+          alt="Mohammad Hatta"
+        />
+        <p>Mohammad Hatta</p>
+      </div>
+      <div className="pahlawan">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Sudirman.jpg/330px-Sudirman.jpg"
+          alt="Jendral Sudirman"
+        />
+        <p>Jendral Sudirman</p>
+      </div>
+    </div>
+  );
+}
